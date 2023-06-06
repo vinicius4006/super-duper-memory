c69725d2d4c256ce4bed03573b91fdb9e790d01d
Vinicius França <vfranca477@gmail.com>
2023-06-05 22:37:38 -0300

    routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import AuthRoutes from "../routes/auth.routes";
-
+import AuthContext from "../contexts/auth";
+import AppRoutes from "./app.routes";
 
 const Routes: React.FC = () => {
-    return <AuthRoutes />;
+  const { signed } = useContext(AuthContext);
+
+  return signed ? <AppRoutes /> : <AuthRoutes />;
 };
 
 export default Routes;
